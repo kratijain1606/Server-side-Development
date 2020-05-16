8d381774702254ed7657d5319d8bcb81fb92f3ea
Krati Jain <kratijain1600@gmail.com>
2020-05-16 17:21:03 +0530

    call back

--- node-js/node-examples/index.js
+++ node-js/node-examples/index.js
@@ -1,17 +1,22 @@
 var rect=require('./rectangle')
-function solveRect(l,b)
-{
-    console.log("Solving for rect with l = "+l +"and b = "+b);
-    if(l<=0 || b<=0)
-    {
-        console.log("Dimension should be greater than 0");
-    }
-    else{
-        console.log("Area of rect = "+rect.area(l,b));
-        console.log("Perimeter of rect = "+rect.perimeter(l,b));
-        
-    }
-}
+
+function solveRect(l,b) {
+    console.log("Solving for rectangle with l = "
+                + l + " and b = " + b);
+    rect(l,b, (err,rectangle) => {
+        if (err) {
+	        console.log("ERROR: ", err.message);
+	    }
+        else {
+            console.log("The area of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
+    console.log("This statement after the call to rect()");
+};
+
 solveRect(2,4);
 solveRect(1,2);
 solveRect(4,6);
