c05ec9acf8c7f8498ade3e0f7d57b8eb0618ac64
Krati Jain <kratijain1600@gmail.com>
2020-06-26 12:20:44 +0530

    passport

--- node-js/confusionServers/app.js
+++ node-js/confusionServers/app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 var session=require('express-session');
 var FileStore=require('session-file-store')(session);
 
-
+var passport=require('passport');
+var authenticate=require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -43,6 +44,9 @@ app.use(session({
   store:new FileStore()
     
 }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -50,12 +54,12 @@ function auth(req,res,next)
 {
   console.log(req.session);
 
-if(!req.session.user)
+if(!req.user)
 {
     var err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
+    // res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 403;
+     next(err);
     // return;
 
 // var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
@@ -73,14 +77,14 @@ if(!req.session.user)
 // }
 }
 else{
-  if(req.session.user==='authenticated'){
+  // if(req.session.user==='authenticated'){
     next();
-  }
-  else{
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    return next(err);
-  }
+  // }
+  // else{
+  //   var err = new Error('You are not authenticated!');
+  //   err.status = 403;
+  //   return next(err);
+  // }
 }
   
 }
